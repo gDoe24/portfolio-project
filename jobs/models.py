744617a780bad5f8ddde1a4cf22b030d2ed614f5
gDoe24 <gregkeel11@gmail.com>
2019-10-12 15:08:20 -0400

    Links to demo and github

--- jobs/models.py
+++ jobs/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Job(models.Model):
 	image = models.ImageField(upload_to='images/')
 	summary = models.CharField(max_length = 200)
-	file = models.FileField(null=True, blank=True)
+	link = models.URLField(max_length=128, blank=True)
+	github= models.URLField(max_length=128, blank=True)
